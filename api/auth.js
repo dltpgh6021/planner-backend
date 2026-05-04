3824c071f44c609feaf37998d5243d8be74c7c23
dltpgh6021 <seholee7003@gmail.com>
2026-05-05 00:47:02 +0900

    test user 생성

--- api/auth.js
+++ api/auth.js
@@ -13,7 +13,7 @@ const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
 router.get('/dev-token', (req, res) => {
     // test 유저의 정보로 JWT 토큰 강제 생성
     const testToken = jwt.sign(
-        { id: "abf32693-ea4f-4422-9323-7bf423e31f45", googld_id: "dummy_google_123" }, 
+        { id: "5ffd29b8-a3cc-4de2-85c8-aabd2178e06d", googld_id: "dummy_google_123" }, 
         process.env.JWT_SECRET, 
         { expiresIn: '7d' }
     );
