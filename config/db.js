d99e3d93ad1859e62f626f785aa9c0198ad7d1f4
seyyoeng46 <seyyoeng46@daum.net>
2026-05-16 00:43:36 +0900

    백엔드 db 테스트용 수정

--- config/db.js
+++ config/db.js
@@ -5,9 +5,9 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL, 
     
     // 아래 ssl 설정 3줄을 추가해 주세요!
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: process.env.DB_SSL === "true"
+        ? { rejectUnauthorized: false }
+    : false
 });
 
 // 예기치 않은 에러 발생 시 처리하는 부분
